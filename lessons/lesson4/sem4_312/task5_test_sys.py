18ffc77a359a1c0182c32ce248b4a0fdeb358321
ShtinRuslan <shtin.ra@gmail.com>
2023-10-07 19:15:07 +0300

    completed test system for task5

--- lessons/lesson4/sem4_312/task5_test_sys.py
+++ lessons/lesson4/sem4_312/task5_test_sys.py
@@ -7,8 +7,15 @@ class test_arg:
 
 
 def parser(string, valid_pairs):
-    pass
-
+    corr_str = string.replace('>', '> ')
+    corr_str = corr_str.replace('<', ' <')
+    msv_str = corr_str.split()
+    ans = []
+    for i in range(len(msv_str)-2):
+        if (msv_str[i], msv_str[i+2]) in valid_pairs:
+            if msv_str[i+1] not in ans:
+                ans.append(msv_str[i+1])
+    return ans
 
 if __name__ == "__main__":
     print()
@@ -20,13 +27,22 @@ if __name__ == "__main__":
         
         test_arg(string='<\a>float<\b>double<\c>complex<c><b><a>',
                  val_pairs=[("<a>", "</a>"), ("<b>", "</b>"), ("<c>", "</c>")],
-                 ans=['complex']),
+                 ans=[]),
 
         test_arg(string="</p>this_is_float<p>",
                  val_pairs=[("</p>", "<p>")],
                  ans=["this_is_float"]),
+
+        test_arg(string="</a>this<a></b>is<b><a>good</a><c>example</c>",
+                 val_pairs=[("<a>", "</a>"),("</a>", "<a>"),("</b>", "<b>"),("<c>", "</c>")],
+                 ans=["this", "is", "good", "example"]),
+        
+        test_arg(string="</a>this<c></b>is<b><a>good</a><a>example</c>",
+                 val_pairs=[("<a>", "</a>"),("</a>", "<a>"),("</b>", "<b>"),("<c>", "</c>")],
+                 ans=["is", "good"]),
     ]
 
+    success_count = 0
     for case in test_cases:
         print(f"string: {case.string}")
         print(f"val_pairs: {case.val_pairs}")
@@ -37,6 +53,8 @@ if __name__ == "__main__":
 
         if  output == case.ans:
             print("OK".center(20,"="), end="\n\n")
+            success_count += 1
         else:
             print("NOT".center(20,"~"), end="\n\n")
-            
+    
+    print(f"tests passed: {success_count}/{len(test_cases)}")
