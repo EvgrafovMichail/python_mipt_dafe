2981712abc0ee7986d0213dd644ede90268996b2
Kynemallv <matveygor41@gmail.com>
2023-10-12 18:13:28 +0300

    sem 5 homework

--- lessons/lesson5/sem5_313/t2_compare.py
+++ lessons/lesson5/sem5_313/t2_compare.py
@@ -2,28 +2,36 @@
 Задача 2: сравнение слов
 
 Вам даны 2 слова: word1 и word2
-Проверьте, возможно ли из уникальных 
+Проверьте, возможно ли из уникальных
 Букв первого слова составить второе.
 """
 
 def is_anagram(word1: str, word2: str) -> bool:
-    """ Проверка возможности составления слова word2
-        из букв слова word1
+    letters = {}
 
-        Вход:
-            word1: str
-                слово, из букв которого надо составить требуемое слово
-            word2: str
-                слово, которое требуется состваить из букв первого слова
-        
-        Выход:
-            is_anagram: bool
-                Отчёт о возможности составления нужного слова.
-                True, если слово word2 можно составить 
-                из уникальных букв слова word1. Иначе, False 
-    """
-    pass
+    if len(word1) != len(word2):
+        return False
 
+    for i in range(len(word1)):
+        letter1 = word1[i]
+        letters[letter1] = letters[letter1] = letters.setdefault(letter1, 0) + 1
+
+        letter2 = word2[i]
+        letters[letter2] = letters.setdefault(letter2, 0) - 1
+
+    return all([val == 0 for val in letters.values()])
 
 if __name__ == "__main__":
-    pass
+    terminal_length = 80
+
+    print(
+            '',
+            'START SCRIPT'.center(terminal_length, '='),
+            sep='\n'
+        )
+    word1 = input("Enter any first string: ")
+    word2 = input("Enter any second string: ")
+    print('\n' + ('Is anagram!' if is_anagram(word1, word2) else 'Is not anagram'))
+    print(
+            'END SCRIPT'.center(terminal_length, '='),
+        )
--- lessons/lesson5/sem5_313/t6_longest_substring.py
+++ lessons/lesson5/sem5_313/t6_longest_substring.py
@@ -31,11 +31,28 @@ def lengthOfLongestSubstring(s: str) -> int:
             subs_len: int
                 длина максимального особого подслова 
     """
-    pass
+    last_index = {}
+    mx = 0
+
+    i = 0
+    while i < len(s):
+        if s[i] in last_index:
+            mx = max(len(last_index), mx)
+            i = last_index[s[i]] + 1
+            last_index = {}
+            continue
+
+        last_index[s[i]] = i
+        i += 1
+
+    return max(mx, len(last_index))
 
 
 if __name__ == "__main__":
-    assert lengthOfLongestSubstring('abcabcbb') == 3
-    assert lengthOfLongestSubstring('bbbbb') == 1
-    assert lengthOfLongestSubstring('pwwkew') == 3     # 'pwke' является подпоследовательностью, 
+    # assert lengthOfLongestSubstring('abcabcbb') == 3
+    # assert lengthOfLongestSubstring('bbbbb') == 1
+    # assert lengthOfLongestSubstring('pwwkew') == 3   # 'pwke' является подпоследовательностью,
                                                        # но не подсловом
+    print(lengthOfLongestSubstring('abcda'))
+    print(lengthOfLongestSubstring('bbbbb'))
+    print(lengthOfLongestSubstring('pwwkew'))
