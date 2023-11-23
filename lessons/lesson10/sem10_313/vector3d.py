1ab2583afec249123968fb54bb2e399ccb73994a
gavkosmig1 <iumanov.vm@phystech.edu>
2023-11-23 23:18:08 +0300

    Update vector3d.py

--- lessons/lesson10/sem10_313/vector3d.py
+++ lessons/lesson10/sem10_313/vector3d.py
@@ -1,86 +1,96 @@
-"""
-- __init__() -  инициализация экземпляра класса. Конструирует новый объект типа Vector3D 
-                из трех чисел с плавающей точкой(float). По умолчанию конструирует нулевой вектор. 
-                Если пользователь попытается инициализировать объект нечисловыми типами, 
-                необходимо бросить исключение;  
-- __repr__() -  возвращает текстовую строку: `'Vector3D(x, y, z)'`, где x, y, z - значения компонент;  
-- __abs__() -   возвращает длину вектора;  
-- __bool__() -  возвращает True, если вектор ненулевой, иначе - False;  
-- __eq__(other) - сравнивает два вектора, возвращает True, если векторы равны покомпонентно, иначе False;  
-- __neg__() -   возвращает новый объект типа Vector3D, компоненты которого равны компонентам данного вектора, 
-                домноженным на минус единицу;  
-- __add__(other) - складывает два вектора, возвращает новый объект типа Vector3D - сумму;  
-- __sub__(other) - вычитает вектор other из данного вектора, возвращает новый объект типа Vector3D - разность;  
-- __mul__(scalar) - умножение вектора на скаляр слева, возвращает новый объект типа Vector3D - произведение;  
-- __rmul__(scalar) - умножение вектора на скаляр справа, возвращает новый объект типа Vector3D - произведение;  
-- __truediv__(scalar) - деление вектора на скаляр, возвращает новый объект типа Vector3D - частное;  
-- dot(other) - возвращает результат скалярного произведения;  
-- cross(other) - возвращает векторное произведение между векторами; 
-
-"""
 from typing import Generator, Any
+from dataclasses import dataclass
+import math
 
-
+@dataclass
 class Vector3D:
-    _x: float
-    _y: float
-    _z: float
-    
-    def __init__(
-        self,
-        x: float = 0,
-        y: float = 0,
-        z: float =0
-    ) -> None:
-        pass
-        
+    _x: float = 0
+    _y: float = 0
+    _z: float = 0
+
+
     def __iter__(self) -> Generator[float, None, None]:
-        pass
-    
-    def __repr__(self) -> str:
-        pass
-    
+        yield from (self.x, self.y, self.z)
+
     def __abs__(self) -> float:
-        pass
-    
+        return sum(x_i ** 2 for x_i in self) ** 0.5
+
     def __bool__(self) -> bool:
-        pass
-    
-    def __eq__(self, other: Any) -> bool:    
-        pass
-    
+        return abs(self) != 0
+
+    def __eq__(self, other: Any) -> bool:
+        self._check_type('==', other)
+        return all(x_i == x_j for x_i, x_j in zip(self, other))
+
     def __neg__(self):
-        pass
-    
+        return self * -1
+
     def __add__(self, other):
-        pass
-    
+        self._check_type('+', other)
+        return Vector3D(
+            *[x_i + x_j for x_i, x_j in zip(self, other)]
+        )
+
     def __sub__(self, other):
-        pass
-    
+        self._check_type('-', other)
+        return self + -other
+
     def __mul__(self, scalar: float):
-        pass
-    
+        scalar = float(scalar)
+        return Vector3D(
+            *[x_i * scalar for x_i in self]
+        )
+
     def __rmul__(self, scalar: float):
-        pass
-    
+        return self * scalar
+
+    def dist(self, other):
+        return ((self.x - other.x) ** 2 + (self.y - other.y) ** 2 + (self.z - other.z) ** 2) ** 0.5
+
     def __truediv__(self, scalar):
-        pass
-    
+        scalar = float(scalar)
+        return self * (1 / scalar)
+
     def dot(self, other) -> float:
-        pass
-    
+        self._check_type('dot', other)
+        return sum(x_i * x_j for x_i, x_j in zip(self, other))
+
     def cross(self, other):
-        pass
-        
+        self._check_type('cross', other)
+        return Vector3D(
+            self._y * other._z - other._y * self._z,
+            self._z * other._x - other._z * self._x,
+            self._x * other._y - other._x * self._y
+        )
+
+    def _check_type(self, operation: str, other: Any):
+        if not isinstance(other, Vector3D):
+            raise TypeError(
+                f'objects of type Vector3D and {type(other).__name__} 
+                do not support operation {operation}'
+            )
+
+    def get_angle(self, other, to_deg=False) -> float:
+        if to_deg:
+            return math.degrees(math.acos(self.dot(other) / (abs(self) * abs(other))))
+        return math.acos(self.dot(other) / (abs(self) * abs(other)))
+
     @property
     def x(self) -> float:
-        pass
-    
+        return self.x
+
     @property
     def y(self) -> float:
-        pass
-    
+        return self.y
+
     @property
     def z(self) -> float:
-        pass
+        return self.z
+
+def dist_to_seg(self: Vector3D, other: Vector3D, dot: Vector3D):
+    if dot(other - self, self - dot) <= 0:
+        return self.dist(dot)
+    elif dot(other - self, other - dot) <= 0:
+        return other.dist(dot)
+    else:
+        return abs((dot - self).cross(other - self)) / abs(other - self)
