2eaa0413267320f88138352e420d9654f6a73e36
EvgrafovMichail <autumnevilmen@gmail.com>
2023-12-01 15:23:09 +0300

    add lesson 12

---
+++ lessons/lesson12/modules_lesson/computitions/cpu.py
@@ -0,0 +1,28 @@
+"""
+CPU computitions mock
+"""
+
+from time import time, sleep
+from itertools import cycle
+
+from modules_lesson.computitions.constants import (
+    WHEEL_SYMBOLS,
+    PAUSE_TIME,
+    RESULT_MOCK,
+)
+
+
+CPU_TIME = 10               # время вычисления на GPU
+
+
+def compute() -> None:
+    wheel = cycle(WHEEL_SYMBOLS)
+    time_start = time()
+
+    while time() - time_start < CPU_TIME:
+        print(f'\rComputing: {next(wheel)};', end='')
+        sleep(PAUSE_TIME)
+
+    print('')
+
+    return RESULT_MOCK
