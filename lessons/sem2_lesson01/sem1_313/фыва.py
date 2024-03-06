6a5a1f34ff165c8794ed3b32c75774024790a7f7
KrasikovViktor <vitya22898@gmail.com>
2024-03-06 15:27:34 +0300

    Commit

---
+++ lessons/sem2_lesson01/sem1_313/фыва.py
@@ -0,0 +1,16 @@
+for x1 in "123":
+    for x2 in "123":
+        for x3 in "123":
+            for x4 in "123":
+                for x5 in "123":
+                    for x6 in "123":
+                        for x7 in "123":
+                            for x8 in "123":
+                                for x9 in "123":
+                                    for x10 in "123":
+                                        for x11 in "123":
+                                            for x12 in "123":
+                                                a = 0
+                                                b = 0
+                                                c = 0
+                                                
