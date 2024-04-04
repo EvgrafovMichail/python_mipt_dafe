02a691a1c795c19c36e3c752d47ac612ae111bd4
GorshkovIoann <143000906+GorshkovIoann@users.noreply.github.com>
2024-04-04 15:59:05 +0300

    Add files via upload

---
+++ python_hw2_1/demoNR.py
@@ -0,0 +1,80 @@
+from typing import Callable
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+from nonparamic_regr import NR
+from metric import MAE, MSE, R_2
+
+
+K_NEIGHBOURS = 8
+POINTS_AMOUNT = 1000
+BOUNDS = (-10, 10)
+FIGSIZE = (16, 8)
+
+
+def visualize_results(
+    axis: plt.Axes,
+    abscissa: list,
+    ordinates: list,
+    predictions: list,
+) -> None:
+    axis.scatter(abscissa, ordinates, label='source', c='royalblue', s=1)
+    axis.plot(abscissa, predictions, label='prediction', c='steelblue')
+
+    axis.set_xlim(min(abscissa), max(abscissa))
+    axis.legend()
+
+
+def get_demonstration(
+    function: Callable[[np.ndarray], np.ndarray],
+    regressors: list[NR],
+) -> None:
+
+    abscissa = np.linspace(*BOUNDS, POINTS_AMOUNT)
+    ordinates = function(abscissa)
+
+    for regressor in regressors:
+
+        regressor.fit(abscissa, ordinates)
+
+    _, axes = plt.subplots(1, 2, figsize=(16, 8))
+
+    for ax, regressor in zip(axes, regressors):
+        predictions = regressor.predict(abscissa)
+
+        ax.set_title(type(regressor).__name__, fontweight='bold')
+        visualize_results(ax, abscissa, ordinates, predictions)
+        print(f'MAE={MAE(predictions, ordinates):.4f}',
+              f'MSE={MSE(predictions, ordinates):.4f}',
+              f'R_2={R_2(predictions, ordinates):.4f}',
+              sep='\n')
+
+    plt.show()
+
+
+def main() -> None:
+    functions = [linear, linear_modulated]
+    regressors = [NR(2, 'l2'), NR()]
+
+    for function in functions:
+
+        get_demonstration(function, regressors)
+
+
+def linear(abscissa: np.ndarray) -> np.ndarray:
+    function_values = 5 * abscissa + 1
+    noise = np.random.normal(size=abscissa.size)
+
+    return function_values + noise
+
+
+def linear_modulated(abscissa: np.ndarray) -> np.ndarray:
+    function_values = np.sin(abscissa) * abscissa
+    noise = np.random.normal(size=abscissa.size)
+
+    return function_values + noise
+
+
+main()
---
+++ python_hw2_1/nonparamic_regr.py
@@ -0,0 +1,62 @@
+from typing import Union
+import numpy as np
+
+
+class NR:
+    _win_size: int
+    _metric: str
+    _is_it_fit: bool = False
+    _abscissa: Union[np.ndarray, None]
+    _ordinates: Union[np.ndarray, None]
+
+    def __init__(self, win_size: int = 4, metric: str = 'l1') -> None:
+        if metric != 'l1' and metric != 'l2':
+            raise TypeError("wrong metric")
+        self._metric = metric
+        if not isinstance(win_size, int):
+            raise TypeError("not integer")
+        if win_size <= 0 or win_size // 1 != win_size:
+            print(
+                f"your win_size are uncorrect(<0 or not integer)."
+                f" We castit into{abs(win_size)//1 + 1}"
+            )
+            self._win_size = abs(win_size)//1 + 1
+
+        else:
+            self._win_size = win_size
+
+    def fit(self, abscissa: np.ndarray, ordinates: np.ndarray) -> None:
+        if abscissa.shape[0] != ordinates.shape[0]:
+            raise RuntimeError("different len")
+        if isinstance(abscissa, np.ndarray) and isinstance(ordinates, np.ndarray):
+            self._abscissa = abscissa
+            self._ordinates = ordinates
+            self._is_it_fit = True
+        else:
+            raise TypeError("there is not np.ndarray")
+
+    def predict(
+            self,
+            abscissa: np.ndarray,
+    ):
+        if not self._is_it_fit:
+            raise RuntimeError("There are no test data")
+        if not isinstance(abscissa, np.ndarray):
+            raise TypeError("not np.array")
+        if len(abscissa.shape) == 1:  # нам в л.бом случае наужен двумерный массив координат
+            abscissa = abscissa.reshape(abscissa.shape[0], 1)
+            self._abscissa = self._abscissa.reshape(self._abscissa.shape[0], 1)
+        if self._metric == 'l1':
+            distances = np.linalg.norm(
+                self._abscissa - abscissa[:, np.newaxis], axis=2)
+        if self._metric == 'l2':
+            distances = np.linalg.norm(
+                self._abscissa - abscissa[:, np.newaxis], axis=2, ord=1)
+
+        k_windows = np.sort(distances).T[self._win_size]
+
+        kerE = np.where(np.abs(distances.T/k_windows).T <= 1,
+                        0.75*(1 - (distances.T/k_windows).T**2), 0)
+        approach = np.sum(self._ordinates * kerE, axis=1) / \
+            np.sum(kerE, axis=1)
+        return approach
