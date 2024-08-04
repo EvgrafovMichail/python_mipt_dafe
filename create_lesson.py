c57eef008c2759743b74b0470c67ebf3e7d199e1
EvgrafovMichail <autumnevilmen@gmail.com>
2024-08-04 19:56:31 +0300

    Update create_lesson script

--- create_lesson.py
+++ create_lesson.py
@@ -1,6 +1,8 @@
 import argparse
 import os
 
+from scripts_utils.lesson_template import LessonTemplateCreator
+
 
 parser = argparse.ArgumentParser(
     description=(
@@ -21,38 +23,9 @@ parser.add_argument(
 )
 
 
-def validate_arguments(args: argparse.Namespace) -> None:
-    limit = 1
-
-    if args.lesson_id < limit:
-        raise ValueError(f"lesson id should be greater than {limit - 1}")
-    
-    if args.sem and args.sem < limit:
-        raise ValueError(f"sem id should be greater than {limit - 1}")
-
-
-def create_lesson_folders(args: argparse.Namespace) -> None:
-    folder_name = f"lesson{f'{args.lesson_id}'.rjust(2, '0')}"
-
-    if args.sem:
-        folder_name = f"sem{args.sem}_{folder_name}"
-
-    path_to_lesson = os.path.join('lessons', folder_name)
-
-    if os.path.exists(path_to_lesson):
-        raise RuntimeError(f'{path_to_lesson} is already exist;')
-
-    groups = ['312', '313', '314']
-
-    for group in groups:
-        path_to_sem = os.path.join(
-            path_to_lesson, f'sem{args.lesson_id}_{group}'
-        )
-
-        os.makedirs(path_to_sem)
-
-
 if __name__ == '__main__':
+    path_to_config = os.path.join("config", "sem.json")
+    lesson_creater = LessonTemplateCreator(path_to_config)
+
     args = parser.parse_args()
-    validate_arguments(args)
-    create_lesson_folders(args)
+    lesson_creater(args.lesson_id, args.sem)
